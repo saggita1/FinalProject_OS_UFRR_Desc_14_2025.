4f3676e97b621ff7ac077d766a7394afc2c95aff
saggita1 <ryanpimentel52@gmail.com>
2025-07-27 21:56:06 -0400

    cpu, memoria e conexões socket

--- ddos/servidor/server.js
+++ ddos/servidor/server.js
@@ -9,6 +9,29 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static("cliente")); // Serve HTML e JS do frontend
+app.get("/stats", (req, res) => {
+    const memoriaTotal = os.totalmem();
+    const memoriaLivre = os.freemem();
+    const usoMemoria = ((memoriaTotal - memoriaLivre) / memoriaTotal) * 100;
+
+    const cpus = os.cpus();
+    const usoCpu = cpus.map(cpu => {
+        const total = Object.values(cpu.times).reduce((acc, tv) => acc + tv, 0);
+        const idle = cpu.times.idle;
+        return ((total - idle) / total) * 100;
+    });
+    const mediaCpu = usoCpu.reduce((a, b) => a + b) / usoCpu.length;
+
+    // Número de conexões Socket.io ativas
+    const totalConexoes = io.engine.clientsCount;
+
+    res.json({
+        cpu: mediaCpu.toFixed(2),
+        memoria: usoMemoria.toFixed(2),
+        conexoes: totalConexoes
+    });
+});
+
 
 io.on("connection", (socket) => {
     console.log("Nova conexão: ", socket.id);
